04b48ca30f7e5388ab40dcda66c3c4c999f4ff35
Sofia-rib18 <sof.ribeiro213@gmail.com>
2021-12-30 11:10:57 +0000

    atualização do router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,18 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import login from '../views/login.vue'
 import register from '../views/registo.vue'
+import classifications from '../views/classifications.vue'
+import filmes from '../views/filmes.vue'
+import quizzes from '../views/quizzes.vue'
+import series from '../views/series.vue'
+import filme from '../views/filme.vue'
+import serie from '../views/serie.vue'
+import mundial from '../views/mundial.vue'
+import nacional from '../views/nacional.vue'
+import profile from '../views/profile.vue'
+import quizz from '../views/quizz.vue'
+import actor from '../views/actor.vue'
+import director from '../views/director.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +33,66 @@ const routes = [
     path: '/',
     name: 'register',
     component: register
+  },
+  {
+    path: '/classifications',
+    name: 'classifications',
+    component: classifications
+  },
+  {
+    path: '/classifications/:mundial',
+    name: 'mundial',
+    component: mundial
+  },
+  {
+    path: '/classifications/:nacional',
+    name: 'nacional',
+    component: nacional
+  },
+  {
+    path: '/filmes',
+    name: 'filmes',
+    component: filmes
+  },
+  {
+    path: '/filmes/:filmeId',
+    name: 'filme',
+    component: filme
+  },
+  {
+    path: '/quizzes',
+    name: 'quizzes',
+    component: quizzes
+  },
+  {
+    path: '/series/:quizzId',
+    name: 'quizz',
+    component: quizz
+  },
+  {
+    path: '/series',
+    name: 'series',
+    component: series
+  },
+  {
+    path: '/series/:serieId',
+    name: 'serie',
+    component: serie
+  },
+  {
+    path: '/:profileId',
+    name: 'profile',
+    component: profile
+  },
+  {
+    path: '/:actorId',
+    name: 'actor',
+    component: actor
+  },
+  {
+    path: '/:directorId',
+    name: 'director',
+    component: director
   },
   {
     path: '/about',
---
+++ src/views/quizz.vue
@@ -0,0 +1,10 @@
+<template>
+  <div><h1>quizz</h1></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
