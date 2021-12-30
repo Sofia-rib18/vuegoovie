259a38f38780e880a8078509a892a408a4fbb53a
Maria Rodrigues <mleonorspr@gmail.com>
2021-12-30 14:11:32 +0000

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,53 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <b-navbar toggleable="lg" type="dark" variant="" id="navbarBackground">
+        <b-navbar-brand href="#">
+          <router-link to="/">
+            <img alt="Goovie logo" src="@/assets/goovie.png" width="107px" height="55px">
+          </router-link>
+        </b-navbar-brand>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav id="navbar">
+            <router-link to="/movies" id="link">Movies</router-link>
+            <router-link to="/series" id="link">Series</router-link>
+            <router-link to="/quizzes" id="link">Quizzes</router-link>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#">
+              <b-icon icon="search" width="30px" height="30px"></b-icon>
+            </b-nav-item>
+            <b-nav-item href="#">
+              <b-icon icon="person-circle" width="35px" height="35px"></b-icon>
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Quicksand&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Quicksand', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
+#link{
   padding: 30px;
+  color: #fef8f8;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+#navbarBackground{
+    background: #070707;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="" id="navbarBackground">
-      <b-navbar-brand href="#">
-        <img
-          alt="Vue logo"
-          src="../assets/goovie.png"
-          width="107px"
-          height="55px"
-        />
-      </b-navbar-brand>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav id="navbar">
-          <b-nav-item href="#">Movies</b-nav-item>
-          <b-nav-item href="#">Series</b-nav-item>
-          <b-nav-item href="#">Quizzes</b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item href="#">
-            <b-icon icon="search" width="30px" height="30px"></b-icon>
-          </b-nav-item>
-          <b-nav-item href="#">
-            <router-link :to="{ name: 'login' }"
-              ><b-icon icon="person-circle" width="35px" height="35px"></b-icon
-            ></router-link>
-          </b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    
   </div>
 </template>
 
@@ -40,13 +11,5 @@ export default {
 </script>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 
-#navbarBackground {
-  background: #070707;
-}
 </style>
